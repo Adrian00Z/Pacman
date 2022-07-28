9090bed348750d67296b34db03f2c9a87db800d2
Adrian Zeitlberger <adrian.zeitlberger@it.htlhl.at>
2022-07-28 20:14:42 +0200

    add class arena

---
+++ src/com/company/Arena.java
@@ -0,0 +1,101 @@
+package com.company;
+
+public enum Arena {
+
+    BasicArena(new int[][]{
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            {0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0},
+            {0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0},
+            {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            {0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0},
+
+            {0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
+            {1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1},
+            {0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0},
+
+            {0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
+            {0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            {0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0},
+            {0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0},
+            {0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0},
+
+            {0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0},
+            {0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+            {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    });
+
+    public static int heightOfField;
+    public static int widthOfField;
+    private int[][] states;
+
+
+    Arena(int[][] fields) {
+        states = fields;
+    }
+
+    public int getRows() {
+        return states.length;
+    }
+
+    public int getCols() {
+        return states[0].length;
+    }
+
+    public int getWidthOfField() {
+        return widthOfField;
+    }
+
+    public int getHeightOfField() {
+        return heightOfField;
+    }
+
+    public int getWidth() {
+        return getCols() * getWidthOfField();
+    }
+
+    public int getHeight() {
+        return getRows() * getHeightOfField();
+    }
+
+    public int getStateOfField(int row, int col) {
+        try {
+            return states[row][col];
+        } catch (java.lang.ArrayIndexOutOfBoundsException e) {
+            if (row < 0){
+                row += getRows();
+            } else if (row >= getRows()){
+                row -= getRows();
+            } else if (col < 0) {
+                col += getCols();
+            } else if (col >= getCols()) {
+                col -= getCols();
+            }
+            return getStateOfField(row, col);
+        }
+    }
+
+    public int getStateOfNextField(int row, int col, Figure.Direction direction) {
+        switch (direction) {
+            case UP -> {
+                row--;
+            }
+            case DOWN -> {
+                row++;
+            }
+            case LEFT -> {
+                col--;
+            }
+            case RIGHT -> {
+                col++;
+            }
+        }
+        return getStateOfField(row, col);
+    }
+}
+
